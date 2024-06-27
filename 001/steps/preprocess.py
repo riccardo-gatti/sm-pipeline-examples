eb3c751940461cd44011fdef472336c7d2a2aeae
riccardo-gatti <rgatti@amazon.it>
2024-06-27 17:04:10 +0200

    Added Finetune Example - Chat Dataset

--- 001/steps/preprocess.py
+++ 001/steps/preprocess.py
@@ -66,7 +66,7 @@ def preprocess_ist(s3_output_path):
         json.dump(template, text_file)
 
     # Create IST dataset
-    dataset_train_ist_df = dataset_training_df[['question', 'correct_answer']]
+    dataset_train_ist_df = dataset_training_df[['question', 'correct_answer']].copy()
     dataset_train_ist_df.to_json(finetuning_dataset_local_path, orient="records", lines=True)
 
     # Create evaluation dataset
@@ -92,3 +92,47 @@ def preprocess_ist(s3_output_path):
             "s3_finetune_dataset_path": finetuning_dataset_s3_path,
             "s3_evaluation_data_location": f"{evaluation_dataset_s3_path}/{evaluation_dataset_filename}"}
 
+
+
+def preprocess_chat(s3_output_path):
+    dataset_path = 'allenai/sciq'
+    dataset = load_dataset(dataset_path)
+
+    dataset_training_df = dataset['train'].to_pandas()
+    dataset_training_df = dataset_training_df.sample(n=400, random_state=42, ignore_index=True)
+
+    finetuning_dataset_filename = "dataset_finetune_chat.jsonl"
+    evaluation_dataset_filename = "dataset_evaluation.jsonl"
+    finetuning_dataset_local_path = f"./output/{dataset_path}/{finetuning_dataset_filename}"
+    evaluation_dataset_local_path = f"./output/{dataset_path}/{evaluation_dataset_filename}"
+
+    # Create CHAT dataset
+    dataset_train_chat_df = dataset_training_df[['question', 'correct_answer']].copy()
+    dataset_str = ""
+    for index, row in dataset_train_chat_df.iterrows():
+        sample = {"dialog": [{"content": row['question'], "role": "user"},
+                             {"content": row['correct_answer'], "role": "assistant"}]}
+        dataset_str += str(sample) + "\n"
+
+    write_to_file(dataset_str, finetuning_dataset_local_path)
+
+    # Create evaluation dataset
+    dataset_evaluation_df = dataset_training_df[['question', 'correct_answer']].copy()
+    dataset_evaluation_df[
+        "question"] = "You are an expert answering science related question.\n\n### Answer this question:\n" + \
+                      dataset_evaluation_df["question"].astype(str)
+    dataset_evaluation_df = dataset_evaluation_df.rename(
+        columns={"correct_answer": "target_output", "question": "model_input"})
+    dataset_evaluation_df.to_json(evaluation_dataset_local_path, orient="records", lines=True)
+
+    finetuning_dataset_s3_path = f"{s3_output_path}/{dataset_path}/finetuning/chat"
+    evaluation_dataset_s3_path = f"{s3_output_path}/{dataset_path}/evaluation"
+
+    print("Uploading finetuning dataset...")
+    S3Uploader.upload(finetuning_dataset_local_path, f"{finetuning_dataset_s3_path}")
+    print("Uploading evaluation dataset...")
+    S3Uploader.upload(evaluation_dataset_local_path, f"{evaluation_dataset_s3_path}")
+
+    return {"s3_output_path": s3_output_path,
+            "s3_finetune_dataset_path": finetuning_dataset_s3_path,
+            "s3_evaluation_data_location": f"{evaluation_dataset_s3_path}/{evaluation_dataset_filename}"}
